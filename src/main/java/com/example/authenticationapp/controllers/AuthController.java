4394c5c5e559bf410e08478ae31428b051c45fe5
Rohanfizz <rohans0501@gmail.com>
2024-12-17 19:45:11 +0530

    dummy commit

--- src/main/java/com/example/authenticationapp/controllers/AuthController.java
+++ src/main/java/com/example/authenticationapp/controllers/AuthController.java
@@ -1,20 +1,23 @@
 package com.example.authenticationapp.controllers;
 
-import java.util.Collections;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.authenticationapp.Repositories.UserRepo;
 import com.example.authenticationapp.models.User;
 import com.example.authenticationapp.pojo.UserLoginDetails;
 import com.example.authenticationapp.pojo.UserSignupDetails;
 import com.example.authenticationapp.services.JwtService;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
@@ -68,4 +71,50 @@ public class AuthController {
     final String token = jwtService.generateToken(user.getId());
     return Collections.singletonMap("token", token);
   }
+
+  @RequestMapping(value = "/refreshToken", method = RequestMethod.GET)
+  public String refreshToken(
+    @RequestHeader("Authorization") String token,
+    HttpServletResponse response
+  ) throws java.io.IOException {
+    if (token == null || token.isEmpty() || !token.startsWith("Bearer ")) {
+      response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid Token");
+    }
+
+    String userEmail = jwtService.validateToken(token);
+    //validate if user is in the db
+
+    // generate new token and return
+
+    return userEmail;
+  }
+
+  @RequestMapping(
+    value = "/forgotPassword/{userEmail}",
+    method = RequestMethod.POST
+  )
+  public String forgotPassword(
+    @RequestParam("userEmail") String email,
+    HttpServletResponse response
+  ) throws IOException {
+    if (email == null || email.isEmpty() || !email.contains("@")) {
+      response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid Email");
+    }
+    // generate passworreset token
+    UUID uuid = new UUID.randomUUID();
+    String passwordResetToken = uuid.toString();
+    User user = userRepository.findByEmail(email);
+    // user.setPasswordResetToken(passwordResetToken);
+    // user.setPasswordResetExpiresAt(System.currentTimeMillis() + 10 * 60 * 1000);
+    return "Check your email for password reset token";
+  }
+  /*@Test
+Step 1: Verify the token
+  // Step 1.1: Is passwordResetExpiresAt done?
+  // Step 1.2: Hash token provided by user and then compare with the one in DB
+  // Step 2: Update the password
+  // Step 3: Reset the passwordResetToken in Db. Why? SO that passwordResetToken is only used 1 time by user
+  // Step 4: Generate a JWT token for the client and respond
+    
+} */
 }
--- src/main/java/com/example/authenticationapp/services/JwtService.java
+++ src/main/java/com/example/authenticationapp/services/JwtService.java
@@ -1,6 +1,8 @@
 package com.example.authenticationapp.services;
 
 import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -83,5 +85,25 @@ public class JwtService {
       return null;
     }
     return username;
+  }
+
+  public static String hashString(String input) {
+    try {
+      // Create a MessageDigest instance for SHA-256
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+      // Perform the hashing
+      byte[] hashBytes = digest.digest(input.getBytes());
+
+      // Convert the byte array to a hex string
+      StringBuilder hexString = new StringBuilder();
+      for (byte b : hashBytes) {
+        hexString.append(String.format("%02x", b));
+      }
+
+      return hexString.toString();
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException("Error initializing SHA-256 algorithm", e);
+    }
   }
 }
